2fd86a6068a304565bfa53cdc75b484acd7a4834
Stellaa24 <stellamaris241@gmail.com>
2022-07-14 10:38:39 -0300

    first commit

---
+++ Primeiro_Projeto/src/module-info.java
@@ -0,0 +1,8 @@
+package Primeiro_Projeto;
+
+public class OlaMundo {
+	
+	public static void main(String[] args) {
+		System.out.print("Olá, mundo!");
+	}
+}
---
+++ primeiro_programa_java/src/com/dio/Gato.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+public class Gato {
+
+	
+
+
+		
+		
+	}
+	
+	/*public Gato (String nome, String cor, Interger idade) {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+		
+	}/* 
+}
